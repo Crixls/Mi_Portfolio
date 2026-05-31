783019d1fb58741cf0baaa1ef8d60cca82c68221
Crixls <crixismundial@gmail.com>
2026-05-31 21:09:58 +0200

    Cambios proyectos

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -31,14 +31,13 @@ const Proyectos = () => {
 
       <section className="min-h-screen flex items-center justify-center bg-black ">
 
-      <div className="grid gap-15 md:m-20 lg:m-30 m-10 lg:grid-cols-2  md:grid-cols-2 md:mt-2 lg:mt-40 sm:grid-cols-1 ">
+      <div className="grid gap-15 md:m-20 lg:m-30 m-10 lg:flex  md:flex md:mt-2 lg:mt-40 sm:grid-cols-1 ">
 
         {/* Tarjeta */}
-
         <div
           className={`
-            backdrop-blur-xl border rounded-2xl p-8 
-            flex flex-col h-full md:p-4 md:min-h-40 md:mt-10 
+            backdrop-blur-xl border rounded-2xl p-8
+            flex flex-col h-full md:p-4 md:min-h-40 md:mt-10 lg:w-200
 
             transition-all duration-300 ease-out
             ${hoverEffect}
@@ -57,73 +56,6 @@ const Proyectos = () => {
           `}
         >
           Proyecto 01
-        </h2>
-
-        <p
-          className={`
-            mt-3 text-base leading-relaxed
-            ${darkMode ? "text-neutral-200" : "text-neutral-700"}
-          `}
-        >
-        Plataforma de comercio electrónico con catálogo de productos, carrito de 
-        compra y flujo de pedido, enfocada en la experiencia de usuario y el desarrollo 
-        de una interfaz funcional, intuitiva y visualmente atractiva.        
-        </p>
-
-        <div
-          className={`
-            mt-6 text-sm
-            ${darkMode ? "text-neutral-300" : "text-neutral-600"}
-          `}
-        >
-          HTML5 · Tailwind CSS · React · PHP · Twig  
-        </div>
-
-        <div className="mt-auto flex justify-center items-center pt-6">
-          <a
-            href="https://github.com/Crixls/E-commerce-platform-Full-Stack-Web-Application"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={`
-              inline-flex items-center gap-2 px-4 py-2 rounded-lg font-medium
-              shadow-md active:scale-95 transition-all duration-200
-
-              ${darkMode
-                ? "bg-white text-black hover:bg-gray-200"
-                : "bg-black text-white hover:bg-gray-700"
-              }
-            `}
-          >
-            Ver
-            <LuExternalLink className="w-5 h-5" />
-          </a>
-        </div>
-
-      </div>
-
-        {/* Tarjeta */}
-        <div
-          className={`
-            backdrop-blur-xl border rounded-2xl p-8
-            flex flex-col h-full md:p-4 md:min-h-40 md:mt-10
-
-            transition-all duration-300 ease-out
-            ${hoverEffect}
-
-            ${darkMode
-              ? "bg-white/10 border-white/20 hover:inset-shadow-[0_0_20px_rgba(255,255,255,0.35)]"
-              : "bg-white border-black/10 hover:shadow-lg"
-            }
-          `}
-        >
-  
-        <h2
-          className={`
-            text-4xl font-light mb-2
-            ${darkMode ? "text-white" : "text-black"}
-          `}
-        >
-          Proyecto 02
         </h2>
 
         <p
